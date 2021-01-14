3eae9271850e7207ed50db37b79d1081656c7061
KurnakovMaksim <maksk210@gmail.com>
2021-01-14 17:32:44 +0900

    Add story model with story category

---
+++ StoryHub.BL/Models/Story.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoryHub.BL.Models
+{
+    public class Story
+    {
+        [Key]
+        public Guid Id { get; private set; }
+        public DateTime DateOfCreate { get; private set; }
+        public string Name { get; private set; }
+        public string Image { get; private set; }
+        public string Text { get; private set; }
+        public int QuantityLikes { get; private set; }
+        public int QuantityDislikes { get; private set; }
+        public StoryCategory Category { get; private set; }
+        public Storyteller Storyteller { get; private set; }
+
+        public Story() 
+        {
+            Id = Guid.NewGuid();
+            DateOfCreate = DateTime.Now;
+        }
+        public Story(string name,
+                     string image,
+                     string text,
+                     int quantityLikes,
+                     int quantityDislikes,
+                     StoryCategory category,
+                     Storyteller storyteller)
+        {
+            Name = name;
+            Image = image;
+            Text = text;
+            QuantityLikes = quantityLikes;
+            QuantityDislikes = quantityDislikes;
+            Category = category;
+            Storyteller = storyteller;
+        }
+    }
+}
---
+++ StoryHub.BL/Models/StoryCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoryHub.BL.Models
+{
+    public class StoryCategory
+    {
+        [Key]
+        public Guid Id { get; private set; }
+        public DateTime DateOfCreate { get; private set; }
+        public string Name { get; private set; }
+
+        public StoryCategory() 
+        {
+            Id = Guid.NewGuid();
+            DateOfCreate = DateTime.Now;
+        }
+        public StoryCategory(string name)
+        {
+            Name = name;
+        }
+    }
+}
