188883a9675c6aab0cf4f3085f2fb4864e2effa4
KurnakovMaksim <maksk210@gmail.com>
2021-01-14 17:33:40 +0900

    Add Db context

---
+++ StoryHub.BL/Db_Context/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoryHub.BL.Models;
+
+namespace StoryHub.BL.Db_Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=StoryHub;Trusted_Connection=True;");
+        }
+
+        public DbSet<Storyteller> Storytellers { get; set; }
+        public DbSet<Story> Stories { get; set; }
+        public DbSet<StoryCategory> StoryCategories { get; set; }
+    }
+}
