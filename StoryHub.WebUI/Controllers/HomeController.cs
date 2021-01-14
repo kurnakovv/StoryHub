88f2c623d0d06604404ac63e9bd9db667e5d2696
KurnakovMaksim <maksk210@gmail.com>
2021-01-14 17:44:53 +0900

    Add list of storytellers names on home page

--- StoryHub.WebUI/Controllers/HomeController.cs
+++ StoryHub.WebUI/Controllers/HomeController.cs
@@ -1,26 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using StoryHub.BL.Services.Abstract;
 using StoryHub.WebUI.Models;
+using System.Diagnostics;
 
 namespace StoryHub.WebUI.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IStorytellerService _storytellerService;
+        private readonly IStorytellerCRUD _storytellerCRUD;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+                              IStorytellerService storytellerService,
+                              IStorytellerCRUD storytellerCRUD)
         {
             _logger = logger;
+            _storytellerService = storytellerService;
+            _storytellerCRUD = storytellerCRUD;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var model = _storytellerService.GetAllStorytellers();
+            return View(model);
         }
 
         public IActionResult Privacy()
