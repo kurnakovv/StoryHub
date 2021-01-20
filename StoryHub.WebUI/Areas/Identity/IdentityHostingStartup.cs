286cb8b0e58ff994a3146666d820e85028aa38c6
KurnakovMaksim <maksk210@gmail.com>
2021-01-20 18:28:51 +0900

    Add initial registration and login

---
+++ StoryHub.WebUI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StoryHub.WebUI.Data;
+
+[assembly: HostingStartup(typeof(StoryHub.WebUI.Areas.Identity.IdentityHostingStartup))]
+namespace StoryHub.WebUI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
