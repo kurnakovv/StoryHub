a13a151480f27df679b0e4c3f06df1a876ca5d79
KurnakovMaksim <maksk210@gmail.com>
2021-02-10 20:15:52 +0900

    Add initial profile for storyteller
    Without design

---
+++ StoryHub.WebUI/Controllers/StorytellerProfileController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoryHub.BL.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoryHub.WebUI.Controllers
+{
+    public class StorytellerProfileController : Controller
+    {
+        private readonly IStorytellerService _storytellerCRUD;
+        public StorytellerProfileController(IStorytellerService storytellerCRUD)
+        {
+            _storytellerCRUD = storytellerCRUD;
+        }
+
+        public IActionResult Index(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var storyteller = _storytellerCRUD.FindStorytellerById(id);
+            return View(storyteller);
+        }
+    }
+}
