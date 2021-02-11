1b0de7a42b8c6de00676347123a630128f590d66
KurnakovMaksim <maksk210@gmail.com>
2021-02-11 19:57:58 +0900

    Edit id type from guid to string

--- StoryHub.BL/Models/Story.cs
+++ StoryHub.BL/Models/Story.cs
@@ -6,21 +6,23 @@ namespace StoryHub.BL.Models
     public class Story
     {
         [Key]
-        public Guid Id { get; private set; }
+        public string Id { get; private set; }
+        [Required]
+        [Display(Name = "Date of create.")]
         public DateTime DateOfCreate { get; private set; }
+        [Required]
         public string Name { get; private set; }
         public string Image { get; private set; }
+        [Required]
         public string Text { get; private set; }
-        public int QuantityLikes { get; private set; }
-        public int QuantityDislikes { get; private set; }
+        [Display(Name = "Quantity likes.")]
+        public int QuantityLikes { get; set; }
+        [Display(Name = "Quantity dislikes.")]
+        public int QuantityDislikes { get; set; }
         public StoryCategory Category { get; private set; }
         public Storyteller Storyteller { get; private set; }
 
-        public Story() 
-        {
-            Id = Guid.NewGuid();
-            DateOfCreate = DateTime.Now;
-        }
+        public Story() { }
         public Story(string name,
                      string image,
                      string text,
@@ -29,6 +31,8 @@ namespace StoryHub.BL.Models
                      StoryCategory category,
                      Storyteller storyteller)
         {
+            Id = Guid.NewGuid().ToString();
+            DateOfCreate = DateTime.Now;
             Name = name;
             Image = image;
             Text = text;
